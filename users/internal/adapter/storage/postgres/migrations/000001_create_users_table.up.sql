4db233fc609fad2bf36dbee468704dab7c278b52
Oscar Ossa <oskrlex17@gmail.com>
2024-12-14 12:49:18 -0500

    fix(migrations): update user roles enum to match proto definitions

--- users/internal/adapter/storage/postgres/migrations/000001_create_users_table.up.sql
+++ users/internal/adapter/storage/postgres/migrations/000001_create_users_table.up.sql
@@ -1,11 +1,11 @@
-CREATE TYPE "users_role_enum" AS ENUM ('admin', 'agent', 'reader');
+CREATE TYPE "users_role_enum" AS ENUM ('ROLE_ADMIN', 'ROLE_AGEST', 'ROLE_READER');
 
 CREATE TABLE "users" (
     "id" BIGSERIAL PRIMARY KEY,
     "name" varchar NOT NULL,
     "email" varchar NOT NULL,
     "password" varchar NOT NULL,
-    "role" users_role_enum DEFAULT 'reader',
+    "role" users_role_enum DEFAULT 'ROLE_READER',
     "created_at" timestamptz NOT NULL DEFAULT (now()),
     "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
